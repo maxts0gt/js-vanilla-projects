f61c144ae3e9adb720411fe6d508747f9d4dfd42
Max <bazarsadtsogtjargal@gmail.com>
2021-12-11 23:33:21 +0900

    generate random users with async/await and add useradd btn

--- 05-DOM-array-methods/script.js
+++ 05-DOM-array-methods/script.js
@@ -9,10 +9,13 @@ let data = [];
 
 // Fetch random user data and add money
 async function getRandomUser() {
-	response = await fetch('https://randomuser.me/api');
-	data = await response.json();
+	const response = await fetch('https://randomuser.me/api');
+	const data = await response.json();
+
+	const user = data.results[0];
+
 	const newUser = {
-		name: `${data.results[0].name.first} ${data.results[0].name.last}`,
+		name: `${user.name.first} ${user.name.last}`,
 		money: Math.floor(Math.random() * 1000000),
 	};
 	addData(newUser);
@@ -26,4 +29,29 @@ getRandomUser();
 // Add new obj to data arr
 function addData(obj) {
 	data.push(obj);
+	updateDOM();
 }
+
+// Update DOM
+function updateDOM(providedData = data) {
+	// Clean main div
+	main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+	providedData.forEach((item) => {
+		console.log(item);
+		const element = document.createElement('div');
+		element.classList.add('person');
+		element.innerHTML = `<strong>${item.name}</strong>${formatMoney(
+			item.money
+		)}`;
+		main.appendChild(element);
+	});
+}
+
+// Format currency
+function formatMoney(number) {
+	return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listeners
+addUserBtn.addEventListener('click', getRandomUser);
